0e7e45d1e027623c613229800df7526383bc965f
werisedd <funkiss883@gmail.com>
2026-07-04 21:16:17 +0300

    Create NoRenderHandler.java

---
+++ src/main/java/com/glitchv1/render/NoRenderHandler.java
@@ -0,0 +1,10 @@
+package com.glitchv1.render;
+
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.util.math.MatrixStack;
+
+public class NoRenderHandler {
+    public static void onWorldRender(MatrixStack matrices, Camera camera) {
+        // Тут можно добавить рендер визуалов
+    }
+}
