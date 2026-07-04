e875eed47e453989e6197da0d5242dfc17d02411
werisedd <funkiss883@gmail.com>
2026-07-04 22:41:34 +0300

    Update ConfigManager.java

--- src/main/java/com/glitchv1/config/ConfigManager.java
+++ src/main/java/com/glitchv1/config/ConfigManager.java
@@ -2,11 +2,7 @@ package com.glitchv1.config;
 
 public class ConfigManager {
     public boolean fullbright = true;
-    public boolean customTime = false;
-    public long customTimeTicks = 6000;
     public boolean noFireOverlay = true;
-    public boolean noTotemParticles = true;
-    public boolean noBreakParticles = true;
     public boolean noHurtCam = true;
     public float gammaValue = 100.0f;
     public boolean devBranding = true;
