6a22bbc95428b5968980b65dd81ba1583f62cc0a
werisedd <funkiss883@gmail.com>
2026-07-04 22:42:44 +0300

    Update MixinGameRenderer.java

--- src/main/java/com/glitchv1/mixin/MixinGameRenderer.java
+++ src/main/java/com/glitchv1/mixin/MixinGameRenderer.java
@@ -2,6 +2,7 @@ package com.glitchv1.mixin;
 
 import com.glitchv1.GlitchV1;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,13 +14,12 @@ public class MixinGameRenderer {
     @Inject(method = "render", at = @At("HEAD"))
     private void onRender(float tickDelta, long startTime, boolean tick, CallbackInfo ci) {
         if (GlitchV1.config.fullbright) {
-            net.minecraft.client.MinecraftClient.getInstance()
-                .options.gamma = GlitchV1.config.gammaValue;
+            net.minecraft.client.MinecraftClient.getInstance().options.gamma = GlitchV1.config.gammaValue;
         }
     }
     
     @Inject(method = "bobView", at = @At("HEAD"), cancellable = true)
-    private void noHurtCam(float f, CallbackInfo ci) {
+    private void noHurtCam(MatrixStack matrices, float f, CallbackInfo ci) {
         if (GlitchV1.config.noHurtCam) {
             ci.cancel();
         }
