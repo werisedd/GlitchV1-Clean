d0d08ca330847bcfb1abb13833a42566d5a16b13
werisedd <funkiss883@gmail.com>
2026-07-05 12:28:42 +0300

    Update MixinKeyboard.java

--- src/main/java/com/glitchv1/mixin/MixinKeyboard.java
+++ src/main/java/com/glitchv1/mixin/MixinKeyboard.java
@@ -11,14 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Keyboard.class)
 public class MixinKeyboard {
-    
     @Inject(method = "onKey", at = @At("HEAD"))
     private void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
         if (key == GLFW.GLFW_KEY_RIGHT_SHIFT && action == GLFW.GLFW_PRESS) {
             MinecraftClient mc = MinecraftClient.getInstance();
-            if (mc.currentScreen == null) {
-                mc.openScreen(new GlitchScreen());
-            }
+            if (mc.currentScreen == null) mc.openScreen(new GlitchScreen());
         }
     }
 }
