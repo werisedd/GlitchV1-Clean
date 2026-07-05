2b3c243aee859476c934028cc7805687bd77c1c1
werisedd <funkiss883@gmail.com>
2026-07-05 12:26:15 +0300

    Update ConfigManager.java

--- src/main/java/com/glitchv1/config/ConfigManager.java
+++ src/main/java/com/glitchv1/config/ConfigManager.java
@@ -6,4 +6,11 @@ public class ConfigManager {
     public boolean noHurtCam = true;
     public float gammaValue = 100.0f;
     public boolean devBranding = true;
+    public boolean customSky = true;
+    public boolean smoothSwing = true;
+    public boolean hitSounds = true;
+    public boolean killSounds = true;
+    public boolean cleanHud = true;
+    public boolean glitchUI = true;
+    public float skyR = 0.05f, skyG = 0.0f, skyB = 0.15f;
 }
